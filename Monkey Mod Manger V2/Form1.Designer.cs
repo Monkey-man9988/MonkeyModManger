9ea229bfaecbe70bc256ecd8665af61ac2c95042
Monkey-man9988 <prajwald285@gmail.com>
2025-03-05 18:26:36 -0500

    Add files via upload

---
+++ Monkey Mod Manger V2/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Monkey_Mod_Manger_V2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.guna2BorderlessForm1 = new Guna.UI2.WinForms.Guna2BorderlessForm(this.components);
+            this.guna2DragControl1 = new Guna.UI2.WinForms.Guna2DragControl(this.components);
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2ControlBox1 = new Guna.UI2.WinForms.Guna2ControlBox();
+            this.guna2Panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // guna2BorderlessForm1
+            // 
+            this.guna2BorderlessForm1.ContainerControl = this;
+            this.guna2BorderlessForm1.DockIndicatorTransparencyValue = 0.6D;
+            this.guna2BorderlessForm1.TransparentWhileDrag = true;
+            // 
+            // guna2DragControl1
+            // 
+            this.guna2DragControl1.DockIndicatorTransparencyValue = 0.6D;
+            this.guna2DragControl1.UseTransparentDrag = true;
+            // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(145)))), ((int)(((byte)(255)))));
+            this.guna2Panel1.Controls.Add(this.guna2ControlBox1);
+            this.guna2Panel1.Controls.Add(this.guna2HtmlLabel1);
+            this.guna2Panel1.Location = new System.Drawing.Point(0, 0);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.Size = new System.Drawing.Size(622, 88);
+            this.guna2Panel1.TabIndex = 0;
+            // 
+            // guna2HtmlLabel1
+            // 
+            this.guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel1.Font = new System.Drawing.Font("Segoe UI Black", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel1.Location = new System.Drawing.Point(36, 12);
+            this.guna2HtmlLabel1.Name = "guna2HtmlLabel1";
+            this.guna2HtmlLabel1.Size = new System.Drawing.Size(540, 39);
+            this.guna2HtmlLabel1.TabIndex = 0;
+            this.guna2HtmlLabel1.Text = "Monkey Mod Manger By MonkeyManGt";
+            // 
+            // guna2Button1
+            // 
+            this.guna2Button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(152)))), ((int)(((byte)(255)))));
+            this.guna2Button1.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.guna2Button1.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.guna2Button1.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.guna2Button1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(215)))), ((int)(((byte)(112)))), ((int)(((byte)(255)))));
+            this.guna2Button1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button1.ForeColor = System.Drawing.Color.White;
+            this.guna2Button1.Location = new System.Drawing.Point(192, 209);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.Size = new System.Drawing.Size(234, 101);
+            this.guna2Button1.TabIndex = 1;
+            this.guna2Button1.Text = "Install Mods";
+            this.guna2Button1.Click += new System.EventHandler(this.guna2Button1_Click);
+            // 
+            // guna2ControlBox1
+            // 
+            this.guna2ControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.guna2ControlBox1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(195)))), ((int)(((byte)(112)))), ((int)(((byte)(255)))));
+            this.guna2ControlBox1.IconColor = System.Drawing.Color.White;
+            this.guna2ControlBox1.Location = new System.Drawing.Point(577, 0);
+            this.guna2ControlBox1.Name = "guna2ControlBox1";
+            this.guna2ControlBox1.Size = new System.Drawing.Size(45, 29);
+            this.guna2ControlBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(622, 395);
+            this.Controls.Add(this.guna2Button1);
+            this.Controls.Add(this.guna2Panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.guna2Panel1.ResumeLayout(false);
+            this.guna2Panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2BorderlessForm guna2BorderlessForm1;
+        private Guna.UI2.WinForms.Guna2DragControl guna2DragControl1;
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox1;
+    }
+}
+
---
+++ Monkey Mod Manger V2/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Monkey_Mod_Manger_V2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private string GetGorillaTagSteamPath()
+        {
+            string steamPath = @"C:\Program Files (x86)\Steam\steamapps\common\Gorilla Tag";
+            return Directory.Exists(steamPath) ? steamPath : null;
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            string gtPath = GetGorillaTagSteamPath();
+            if (gtPath == null)
+            {
+                MessageBox.Show("Gorilla Tag (Steam) not found!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            using (WebClient client = new WebClient())
+            {
+                try
+                {
+                    // Define download URLs for individual files
+                    string downloadWimUrl = "https://cdn.discordapp.com/attachments/1176645226153132172/1346970900767051867/winhttp.dll?ex=67ca1fe8&is=67c8ce68&hm=4d897fdbf51070ca4f7c7e94aeceded68ef836dbfca4b409cbca9c7dd878b8f4&";
+                    string downloadDoorStopVUrl = "https://cdn.discordapp.com/attachments/1176645226153132172/1346970923915546665/doorstop_version?ex=67ca1fed&is=67c8ce6d&hm=3ca20457bebccf4d9ea09905dc4f0117bf8370ba66e08feea273afeb1693137e&";
+                    string downloadDoorStopConUrl = "https://cdn.discordapp.com/attachments/1176645226153132172/1346970953544106065/doorstop_config.ini?ex=67ca1ff4&is=67c8ce74&hm=5005dfd2612238678246e6781c4ce883a4901155698a2b84b97cc0f0a3b8999a&";
+                    string downloadChangeLogUrl = "https://cdn.discordapp.com/attachments/1176645226153132172/1346970991322075197/changelog.txt?ex=67ca1ffd&is=67c8ce7d&hm=3879973b46695ec38620e3a6a3a8af1434cd694bf4c6cfcc7b8cf494a6465a6e&";
+
+                    // Define file destinations for individual files
+                    string fileWinHttp = Path.Combine(gtPath, "winhttp.dll");
+                    string fileDoorStopV = Path.Combine(gtPath, ".doorstop_version");
+                    string fileDoorStopCon = Path.Combine(gtPath, "doorstop_config.ini");
+                    string fileChangeLog = Path.Combine(gtPath, "changelog.txt");
+
+                    // Download individual files
+                    client.DownloadFile(downloadWimUrl, fileWinHttp);
+                    client.DownloadFile(downloadDoorStopVUrl, fileDoorStopV);
+                    client.DownloadFile(downloadDoorStopConUrl, fileDoorStopCon);
+                    client.DownloadFile(downloadChangeLogUrl, fileChangeLog);
+
+                    MessageBox.Show("Download complete! Files placed in Gorilla Tag folder.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    // Define ZIP download URL and file paths
+                    string BepInExDownloadUrl = "https://cdn.discordapp.com/attachments/1176645226153132172/1346975499536044033/BepInEx.zip?ex=67ca2430&is=67c8d2b0&hm=ccf79b53fc75bdc17076d09543854d69fcf75f734bc1327b0d6f34aa7bcbbeda&";
+                    string FilePath = Path.Combine(gtPath, "BepInEx.zip"); // Save ZIP in GT folder
+                    
+
+
+                    // Download the BepInEx file
+                    client.DownloadFile(BepInExDownloadUrl, FilePath);
+                    MessageBox.Show("BepInEx Download complete!", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Operation failed: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+        }
+    }
+}
